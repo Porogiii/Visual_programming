e317643bc103a1da53ce4492e507dff9d011c3bf
Артём Русецкий <artemrusetskiy2004@gmail.com>
2024-02-12 01:02:19 +0700

    Add lab_2

---
+++ lab_2/AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    public void Button_Click(object sender, RoutedEventArgs args)
+    {
+        if (sender is Button button)
+        {
+            Canvas.Background = button.Background;
+        }
+    }
+}
